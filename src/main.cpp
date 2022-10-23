a6de56881a75caa22dbaf5c391a4ca423a445e85
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-10-24 00:28:31 +0330

    Create a new PIO project and run Blink example

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(1, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(1, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
